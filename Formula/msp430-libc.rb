65c49dc2dcbc753393ad7dad9e303161bdcbad7d
Adrian Sampson <adrian@radbox.org>
2013-06-21 16:42:32 -0700

    fix libc and mcu formulae

--- Formula/msp430-libc.rb
+++ Formula/msp430-libc.rb
@@ -3,8 +3,14 @@ require 'formula'
 class Msp430Libc < Formula
   homepage 'http://mspgcc.sourceforge.net'
   url 'http://sourceforge.net/projects/mspgcc/files/msp430-libc/msp430-libc-20120224.tar.bz2'
+  sha1 'd01cf0db41bff1a0ab50644cbb11bc5a1d5be172'
+
+  # depends_on 'msp430-gcc'
+
   def install
-    system "make"
-    system "make", "PREFIX=#{prefix}", "install"
+    Dir.chdir 'src' do
+      system "make"
+      system "make", "PREFIX=#{prefix}", "install"
+    end
   end
 end
--- Formula/msp430-mcu.rb
+++ Formula/msp430-mcu.rb
@@ -3,8 +3,15 @@ require 'formula'
 class Msp430Mcu < Formula
   homepage 'http://mspgcc.sourceforge.net'
   url 'http://sourceforge.net/projects/mspgcc/files/msp430mcu/msp430mcu-20120406.tar.bz2'
+  sha1 'c096eec84f0f287c45db713a550ec50c518fa065'
 
   def install
+    # Create the "bin" directory for installation. The install.sh script
+    # expects this to exist.
+    Dir.mkdir "#{prefix}/bin"
+
+    # As suggested by the README.
+    ENV['MSP430MCU_ROOT'] = Dir.pwd
     system "./scripts/install.sh", prefix
   end
 end
