762fd009bac7799be33bcaea6473b46f0d9c717e
bz007 <bz@var.ru>
2014-11-23 11:17:19 +0300

    Update patch checksum

--- msp430-binutils.rb
+++ msp430-binutils.rb
@@ -13,13 +13,14 @@ class Msp430Binutils < Formula
 
   patch do
     url "http://sourceforge.net/projects/mspgcc/files/Patches/binutils-2.22/msp430-binutils-2.22-20120911.patch/download"
+    sha1 "5b3aec605f85fea81a22aa52e9900a5e2f9cc460"
   end
 
   def install
     mkdir 'build' do
-        system "../configure", "--target=msp430", "--program-prefix='msp430-'", "--prefix=#{prefix}"
-        system "make"
-        system "make install"
+      system "../configure", "--target=msp430", "--program-prefix='msp430-'", "--prefix=#{prefix}"
+      system "make"
+      system "make install"
     end
   end
 end
