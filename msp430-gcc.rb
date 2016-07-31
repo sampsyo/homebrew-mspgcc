828bf3f395599d4578d2d3d9e8434edfd2b291e7
Tommy Sparber <tommy@sparber.net>
2016-07-31 21:47:49 +0200

    Update formulae as suggested by brew style and brew audit
    Use URLs as suggested by brew audit:
    Always use https://downloads.sourceforge.net and
    https://ftpmirror.gnu.org to get the closest mirror.
    
    Require ‘formula’ is not needed anymore.
    Always use double quotes.
    Fix some code style warnings.

--- msp430-gcc.rb
+++ msp430-gcc.rb
@@ -1,19 +1,17 @@
-require 'formula'
-
 class Msp430Gcc < Formula
-  homepage 'http://mspgcc.sourceforge.net'
-  url 'http://www.netgull.com/gcc/releases/gcc-4.7.0/gcc-4.7.0.tar.bz2'
+  homepage "http://mspgcc.sourceforge.net"
+  url "https://ftpmirror.gnu.org/gcc/gcc-4.7.0/gcc-4.7.0.tar.bz2"
   sha256 "a680083e016f656dab7acd45b9729912e70e71bbffcbf0e3e8aa1cccf19dc9a5"
   env :std
 
-  depends_on 'msp430-binutils'
-  depends_on 'mpfr'
-  depends_on 'gmp'
-  depends_on 'isl'
-  depends_on 'libmpc'
+  depends_on "msp430-binutils"
+  depends_on "mpfr"
+  depends_on "gmp"
+  depends_on "isl"
+  depends_on "libmpc"
 
   patch do
-    url "http://sourceforge.net/projects/mspgcc/files/Patches/gcc-4.7.0/msp430-gcc-4.7.0-20120911.patch/download"
+    url "https://downloads.sourceforge.net/project/mspgcc/Patches/gcc-4.7.0/msp430-gcc-4.7.0-20120911.patch"
     sha256 "db0b6e502c89be4cfee518e772125eaea66cc289d9428c57ddcc187a3be9e77a"
   end
 
@@ -23,18 +21,17 @@ class Msp430Gcc < Formula
     # configure: error: cannot compute suffix of object files: cannot compile
     # which, upon further inspection, arises when xgcc bails out when it sees
     # this argument.
-    ENV.remove_from_cflags '-Qunused-arguments'
+    ENV.remove_from_cflags "-Qunused-arguments"
     ENV.remove_from_cflags(/ ?-march=\S*/)
     ENV.remove_from_cflags(/ ?-msse[\d\.]*/)
     ENV.remove_from_cflags(/ ?-mmacosx-version-min=10\.\d+/)
 
     # gcc must be built outside of the source directory.
-    mkdir 'build' do
-      binutils = Formula.factory('msp430-binutils')
-      cc = ENV['CC']
+    mkdir "build" do
+      binutils = Formula["msp430-binutils"]
       system "../configure", "--target=msp430", "--enable-languages=c,c++", "--program-prefix='msp430-'", "--prefix=#{prefix}", "--with-as=#{binutils.opt_prefix}/msp430/bin/as", "--with-ld=#{binutils.opt_prefix}/msp430/bin/ld"
       system "make"
-      system "make install"
+      system "make", "install"
 
       # Remove libiberty, which conflicts with the same library provided by
       # binutils.
@@ -42,8 +39,8 @@ class Msp430Gcc < Formula
       # Fix inspired by:
       # https://github.com/larsimmisch/homebrew-avr/commit/8cc2a2e591b3a4bef09bd6efe2d7de95dfd92794
       multios = `gcc --print-multi-os-directory`.chomp
-      libiberty = "#{prefix}/lib/#{multios}/libiberty.a"
-      if File.exists?(libiberty)
+      libiberty = "#{lib}/#{multios}/libiberty.a"
+      if File.exist?(libiberty)
         File.unlink libiberty
       end
     end
--- msp430-mcu.rb
+++ msp430-mcu.rb
@@ -1,17 +1,15 @@
-require 'formula'
-
 class Msp430Mcu < Formula
-  homepage 'http://mspgcc.sourceforge.net'
-  url 'http://sourceforge.net/projects/mspgcc/files/msp430mcu/msp430mcu-20120406.tar.bz2'
+  homepage "http://mspgcc.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/mspgcc/msp430mcu/msp430mcu-20120406.tar.bz2"
   sha256 "0637014e8e509746c3f6df8e1d65b786770d162b3a0b86548bdf76ac3102c96e"
 
   def install
     # Create the "bin" directory for installation. The install.sh script
     # expects this to exist.
-    Dir.mkdir "#{prefix}/bin"
+    Dir.mkdir bin.to_s
 
     # As suggested by the README.
-    ENV['MSP430MCU_ROOT'] = Dir.pwd
+    ENV["MSP430MCU_ROOT"] = Dir.pwd
     system "./scripts/install.sh", prefix
   end
 end
