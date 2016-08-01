e080e2a47de734d3bc20e0011962d05eeac016c5
Tommy Sparber <tommy@sparber.net>
2016-08-01 13:38:20 +0200

    Update msp430-mcu to 20130321

--- msp430-mcu.rb
+++ msp430-mcu.rb
@@ -1,7 +1,7 @@
 class Msp430Mcu < Formula
   homepage "http://mspgcc.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/mspgcc/msp430mcu/msp430mcu-20120406.tar.bz2"
-  sha256 "0637014e8e509746c3f6df8e1d65b786770d162b3a0b86548bdf76ac3102c96e"
+  url "https://downloads.sourceforge.net/project/mspgcc/msp430mcu/msp430mcu-20130321.tar.bz2"
+  sha256 "01be411f8901362fab32e7e1be044a44e087a98f3bd2346ac167960cfe6fa221"
 
   def install
     # Create the "bin" directory for installation. The install.sh script
